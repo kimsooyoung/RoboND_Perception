a60a8006637206b6d9a1807b9ff88d8056618e10
kimsooyoung <tge1375@naver.com>
2023-01-12 13:41:28 +0900

    update in linux

--- Calibration, Filtering, and Segmentation/ex1_findChessboardCorners.py
+++ Calibration, Filtering, and Segmentation/ex1_findChessboardCorners.py
@@ -10,21 +10,24 @@ ny = 6#TODO: enter the number of inside corners in y
 
 # Make a list of calibration images
 images = glob.glob('./cali_img/*.jpg')
-# Select any index to grab an image from the list
-idx = 0
-# Read in the image
-img = mpimg.imread(images[idx])
-plt.imshow(img)
 
-# Convert to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-# Find the chessboard corners
-ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-
-# If found, draw corners
-if ret == True:
-    # Draw and display the corners
-    print("corner found")
-    cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
+for i in range(len(images)):
+    # Select any index to grab an image from the list
+    idx = 0
+    # Read in the image
+    img = mpimg.imread(images[i])
     plt.imshow(img)
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+    # If found, draw corners
+    if ret == True:
+        # Draw and display the corners
+        print("corner found")
+        cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
+        plt.imshow(img)
+        plt.show()
